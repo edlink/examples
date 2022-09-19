198096037a51e132a58821fe4b1bd73b6b443899
Dakota Gordon <dakota@via.world>
2022-09-19 11:06:49 -0500

    Updated Graph API example to fix a couple minor errors.

--- graph-api/src/main.ts
+++ graph-api/src/main.ts
@@ -1,3 +1,15 @@
+/**
+ * This file demonstrates how to query each of the Edlink Graph API entity endpoints
+ * and retrieve a full copy of the source dataset, which can then be written to
+ * a database and saved / queried.
+ * 
+ * The only variable that you need to fill in is ths the integration access token,
+ * which you can find on your Edlink dashboard, or programmatically via our API.
+ * See the developer guide on "Configuring Integrations via API" for more details.
+ * 
+ * We use the axios library to make HTTP requests, but you can use any library.
+ */
+
 import axios from 'axios';
 
 const header_config = {
@@ -18,11 +30,6 @@ const entity_types = [
     'agents'
 ];
 
-/**
- * This file demonstrates how to query each of the Edlink Graph API entity endpoints
- * and retrieve a full copy of the source dataset, which can then be written to
- * a database and saved / queried.
- */
 async function loadSourceDataset() {
     const source_dataset = {
         districts: [],
@@ -37,26 +44,30 @@ async function loadSourceDataset() {
     };
 
     for (const entity_type of entity_types) {
+        // This is our initial request to the API. Note the $first parameter, which indicates both the order and quantity of results.
+        let url = `https://ed.link/api/v2/graph/${entity_type}?$first=1000`;
 
-        let url = `https://ed.link/api/v2/graph/${entity_type}$first=10000`;
         while(url) {
-            const response = await axios
-                .get(`https://ed.link/api/v2/graph/${entity_type}$first=10000`, header_config)
+            const { $data, $next } = await axios.get(url, header_config)
                 .then((response) => response.data);
 
-                for (const entity of response.$data) {
+                for (const entity of $data) {
                     source_dataset[entity_type].push(entity);
                 }
 
-            url = response.$next;
+            // Edlink will respond with a cursor to the next page of results, if there are any.
+            // If this is the last page, $next will be null and this loop will terminate.
+            url = $next;
         }
     }
 
+    // At this point, we will have the whole dataset in memory. We can now write it to a database, or do whatever else we need to do.
     writeToDB(source_dataset);
 }
 
 async function writeToDB(source_dataset) {
-    // Perform creates, updates, and deletes on the database here.
+    // This function you'll want to implement on your own.
+    // It should perform creates, updates, and deletes on your database (or something similar).
 }
 
 loadSourceDataset();
